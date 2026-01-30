f1831e01c38ec8bbcbdaa1cfb331b310fd021476
Yashi Bansal <bansalyashi163@gmail.com>
2026-01-30 21:56:56 +0530

    hero-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import HeroSection from "@/components/HeroSection";
 import { main } from "framer-motion/client";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]"></main>
+    <main className=" min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <HeroSection/>
+    </main>
   );
 }
